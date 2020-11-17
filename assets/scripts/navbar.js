b832df6afb13c65913b9c3aea4f9f4da79749167
ducktrshessami <jwee761@gmail.com>
2020-11-16 17:23:40 -0800

    navbar structure

---
+++ assets/scripts/navbar.js
@@ -0,0 +1,8 @@
+const options = {
+    edge: "right"
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+});
