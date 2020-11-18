7c700fc16b5066b35147cdd7ff3a81f000f0cc44
ducktrshessami <jwee761@gmail.com>
2020-11-17 18:23:38 -0800

    auto age display

---
+++ assets/scripts/index.js
@@ -0,0 +1,9 @@
+(function() { // Enclose scope
+    const birthday = new Date("7/9/1998");
+
+    // Auto birthday display
+    $("#age").text((function() {
+        let now = new Date();
+        return now.getFullYear() - birthday.getFullYear() - (birthday.getMonth() < now.getMonth() || (birthday.getMonth() == now.getMonth() && birthday.getDate() <= now.getDate()) ? 0 : 1);
+    })());
+})();
--- assets/scripts/navbar.js
+++ assets/scripts/navbar.js
@@ -2,6 +2,7 @@ const options = {
     edge: "right"
 };
 
+// Sidebar nav init
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, options);
