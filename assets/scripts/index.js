f126b18f9504dd479f1d29cc73207cc6616cf363
ducktrshessami <jwee761@gmail.com>
2020-11-17 20:46:34 -0800

    mobile about me

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,7 +2,7 @@
     const birthday = new Date("7/9/1998");
 
     // Auto birthday display
-    $("#age").text((function() {
+    $(".age-display").text((function() {
         let now = new Date();
         return now.getFullYear() - birthday.getFullYear() - (birthday.getMonth() < now.getMonth() || (birthday.getMonth() == now.getMonth() && birthday.getDate() <= now.getDate()) ? 0 : 1);
     })());
