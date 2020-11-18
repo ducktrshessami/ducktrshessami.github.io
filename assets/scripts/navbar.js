8c22ff35cf575eba2c63a6f76282ca036b3935a2
ducktrshessami <jwee761@gmail.com>
2020-11-17 19:59:34 -0800

    enclose scope of navbar handling

--- assets/scripts/navbar.js
+++ assets/scripts/navbar.js
@@ -1,9 +1,11 @@
-const options = {
-    edge: "right"
-};
-
-// Sidebar nav init
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-});
+(function() { // Enclose scope
+    const options = {
+        edge: "right"
+    };
+    
+    // Sidebar nav init
+    document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.sidenav');
+        var instances = M.Sidenav.init(elems, options);
+    });
+})();
