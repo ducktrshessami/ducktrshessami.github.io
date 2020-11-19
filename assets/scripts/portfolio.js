f1c0f652b046df1a4e3f902d15e9176d5efd9448
ducktrshessami <jwee761@gmail.com>
2020-11-19 13:46:55 -0800

    portfolio content

--- assets/scripts/portfolio.js
+++ assets/scripts/portfolio.js
@@ -9,9 +9,28 @@
         },
         {
             title: "Up Next",
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium placeat voluptas odit asperiores dolorem aspernatur illum hic totam debitis voluptatum rerum quibusdam quas reiciendis excepturi optio veniam, blanditiis, labore consequatur!\nLorem ipsum dolor sit amet consectetur adipisicing elit. Natus, incidunt ratione! Blanditiis repudiandae corrupti vero accusantium nisi, saepe, ipsum illo pariatur iure dolorum nemo eveniet. Eius vero iste voluptatibus illum.",
+            description: "A web application to locate local shows",
             langs: ["js", "html", "css"],
             url: "https://github.com/ducktrshessami/up-next"
+        },
+        {
+            title: "Weather Dashboard",
+            description: `
+                A centralized dashboard to keep track of multiple locations' weather conditions
+                Weather condition data and icons by OpenWeather
+            `,
+            langs: ["js", "html", "css"],
+            url: "https://github.com/ducktrshessami/weather-dashboard"
+        },
+        {
+            title: "Work Day Scheduler",
+            description: `
+                A day planner for a single day's business hours.
+                Time blocks are color-coded relative to the current time.
+                Notes can be saved and stored locally.
+            `,
+            langs: ["js", "html", "css"],
+            url: "https://github.com/ducktrshessami/work-day-scheduler"
         }
     ];
     const langImgs = {
@@ -25,18 +44,19 @@
     /*
     Create a card given a portfolio item's information
 
-    @param data: {
+    @param data: an object containing {
         title: string,
+        image: a string containing an image URL,
         description: string,
-        langs: Array<string>
+        langs: an Array<string> containing a list of programming languages used
         url: string
     }
 
     @return a jQuery object containing the created element
     */
     function portfolioCard(data) {
-        let parsedDesc = data.description.trim().split('\n').map(line => `<p>${line}</p>`).join("\n<br>\n");
-        let parsedLangs = data.langs.map(lang => `<li class="lang"><img src="${langImgs[lang]}" alt="${lang}"></li>`).join('\n');
+        let parsedDesc = data.description.trim().split('\n').map(line => `<p>${line.trim()}</p>`).join("\n<br>\n");
+        let parsedLangs = data.langs.map(lang => `<li class="lang"><img src="${langImgs[lang.trim()]}" alt="${lang.trim()}"></li>`).join('\n');
         return $(`
             <li class="portfolio-item">
                 <a href="${data.url}" class="portfolio-card card blue-grey darken-4 white-text waves-effect waves-light">
