ea41808d9947ea9dadf746268aaefd4663bba29e
ducktrshessami <jwee761@gmail.com>
2021-03-02 22:24:44 -0800

    added alt tags to images

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -3,7 +3,7 @@ export default function Index() {
         <article className="row">
             <div className="col s12 m8 offset-m2 white-text">
                 <div className="card blue-grey darken-4 horizontal hide-on-small-only">
-                    <img src="/logo512.png" className="card-image horizontal-image" />
+                    <img src="/logo512.png" alt="Logo" className="card-image horizontal-image" />
                     <div className="card-stacked">
                         <div className="card-content">
                             <h1 className="card-title">About Me</h1>
@@ -26,7 +26,7 @@ export default function Index() {
                     </div>
                 </div>
                 <div className="card blue-grey darken-4 hide-on-med-and-up">
-                    <img src="/logo512.png" className="card-image vertical-image" />
+                    <img src="/logo512.png" alt="Logo" className="card-image vertical-image" />
                     <div className="card-content">
                         <p>Josh Wee (He/Him)</p>
                         <p>Age: <span className="age-display"></span></p>
