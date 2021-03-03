8bfbc1835c34c54e12f45e605a3436819432ed01
ducktrshessami <jwee761@gmail.com>
2021-03-02 21:22:31 -0800

    converted other pages to jsx

---
+++ src/pages/contact.js
@@ -0,0 +1,30 @@
+export default function Contact() {
+    return (
+        <section class="row">
+            <div class="card blue-grey darken-4 white-text col s12 m8 offset-m2 l6 offset-l3">
+                <div class="card-content">
+                    <div class="hide-on-small-only">
+                        <h1 class="card-title">Contact</h1>
+                        <hr />
+                        <br />
+                    </div>
+                    <form id="contact-form">
+                        <div class="input-field">
+                            <input type="text" id="name" name="name" class="validate white-text" />
+                            <label for="name">Name</label>
+                        </div>
+                        <div class="input-field">
+                            <input type="text" name="email" id="email" class="validate white-text" />
+                            <label for="email">E-Mail</label>
+                        </div>
+                        <div class="input-field">
+                            <textarea name="message" id="message" class="materialize-textarea validate white-text"></textarea>
+                            <label for="message">Message</label>
+                        </div>
+                        <button type="submit" form="contact-form" id="submit" class="btn waves-effect waves-dark black-text">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </section>
+    );
+};
---
+++ src/pages/portfolio.js
@@ -0,0 +1,9 @@
+export default function Portfolio() {
+    return (
+        <section class="row">
+            <div class="col s12 m8 offset-m2">
+                <ul id="portfolio-list"></ul>
+            </div>
+        </section>
+    );
+};
