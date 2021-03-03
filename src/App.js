5b6dccc7b67a35a2aa05724654d16b346155c483
ducktrshessami <jwee761@gmail.com>
2021-03-02 19:31:41 -0800

    imported materialize for whole app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import "materialize-css/dist/css/materialize.min.css";
 
 function App() {
   return (
