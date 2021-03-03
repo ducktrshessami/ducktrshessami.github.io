2448d1f74f8823ad32844c7bf1d756c7712edcce
ducktrshessami <jwee761@gmail.com>
2021-03-02 22:21:20 -0800

    updated page imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from "react-router-dom";
 
-import Index from "./pages/index";
-import Portfolio from "./pages/portfolio";
-import Contact from "./pages/contact";
+import Index from "./pages/Index";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
