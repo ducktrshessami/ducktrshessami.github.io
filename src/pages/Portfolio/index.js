fe48c7f210accd60f0a269fe75a1dc3f18d2de25
ducktrshessami <jwee761@gmail.com>
2021-03-03 18:45:12 -0800

    convert Portfolio page to Component class

--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -1,11 +1,27 @@
+import { Component } from "react";
 import Project from "../../components/Project";
+import "./Portfolio.css";
 
-export default function Portfolio() {
-    return (
-        <section class="row">
-            <div class="col s12 m8 offset-m2">
-                <ul id="portfolio-list"></ul>
-            </div>
-        </section>
-    );
-};
+const initialProjects = 6; // Number of projects displayed initially
+const clickMeSeconds = 5; // Number of seconds the intial help message is shown
+
+export default class Portfolio extends Component {
+    state = {
+        shown: this.props.projects.slice(0, initialProjects),
+        all: this.props.projects
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <section class="row">
+                <div class="col s12 m8 offset-m2">
+                    <ul id="portfolio-list">{this.state.shown.map(Project)}</ul>
+                </div>
+            </section>
+        );
+    }
+}
