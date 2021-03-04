8a4b0bf509e5b366f3be9393854fa543c8959869
Josh Fish <jwee761@gmail.com>
2021-03-03 16:16:14 -0800

    copied index header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,34 @@
+import {
+    Route,
+    Link
+} from "react-router-dom";
+import M from "materialize-css";
+
+import "./Header.css";
+
 export default function Header() {
     return (
         <header>
-            
+            <nav className="nav-wrapper">
+                <Link to="/" className="left hide-on-small-only">
+                    <img src="/logo192.png" alt="Logo" className="logo circle middle" />
+                    <span className="active">ducktrshessami</span>
+                </Link>
+                <ul className="left hide-on-small-only">
+                    <li><Link to="/portfolio">Portfolio</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                </ul>
+                <span className="brand-logo center hide-on-med-and-up">About Me</span>
+                <a role="button" data-target="sidenav" className="sidenav-trigger right hide-on-med-and-up"><i className="material-icons">menu</i></a>
+            </nav>
+            {M.Sidenav.init(
+                <ul id="sidenav" className="sidenav">
+                    <li><Link className="disabled">About Me</Link></li>
+                    <li><Link to="/portfolio">Portfolio</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                </ul>,
+                { edge: "right" }
+            )}
         </header>
     );
 }
