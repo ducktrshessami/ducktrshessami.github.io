a77010a27d71064ff7b6e25d245082183295ea83
Josh Fish <jwee761@gmail.com>
2021-03-03 16:19:31 -0800

    Header extend Component class

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,3 +1,4 @@
+import { Component } from "react";
 import {
     Route,
     Link
@@ -6,29 +7,28 @@ import M from "materialize-css";
 
 import "./Header.css";
 
-export default function Header() {
-    return (
-        <header>
-            <nav className="nav-wrapper">
-                <Link to="/" className="left hide-on-small-only">
-                    <img src="/logo192.png" alt="Logo" className="logo circle middle" />
-                    <span className="active">ducktrshessami</span>
-                </Link>
-                <ul className="left hide-on-small-only">
-                    <li><Link to="/portfolio">Portfolio</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
-                </ul>
-                <span className="brand-logo center hide-on-med-and-up">About Me</span>
-                <a role="button" data-target="sidenav" className="sidenav-trigger right hide-on-med-and-up"><i className="material-icons">menu</i></a>
-            </nav>
-            {M.Sidenav.init(
+export default class Header extends Component {
+    render() {
+        return (
+            <header>
+                <nav className="nav-wrapper">
+                    <Link to="/" className="left hide-on-small-only">
+                        <img src="/logo192.png" alt="Logo" className="logo circle middle" />
+                        <span className="active">ducktrshessami</span>
+                    </Link>
+                    <ul className="left hide-on-small-only">
+                        <li><Link to="/portfolio">Portfolio</Link></li>
+                        <li><Link to="/contact">Contact</Link></li>
+                    </ul>
+                    <span className="brand-logo center hide-on-med-and-up">About Me</span>
+                    <a role="button" data-target="sidenav" className="sidenav-trigger right hide-on-med-and-up"><i className="material-icons">menu</i></a>
+                </nav>
                 <ul id="sidenav" className="sidenav">
                     <li><Link className="disabled">About Me</Link></li>
                     <li><Link to="/portfolio">Portfolio</Link></li>
                     <li><Link to="/contact">Contact</Link></li>
-                </ul>,
-                { edge: "right" }
-            )}
-        </header>
-    );
-}
+                </ul>
+            </header>
+        );
+    }
+};
