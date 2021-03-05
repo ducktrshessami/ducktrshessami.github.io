cbbde157d9d2d974eed46697df3ead9fb711f0c5
ducktrshessami <jwee761@gmail.com>
2021-03-04 20:13:36 -0800

    added react language badge

--- src/components/Project/Lang.js
+++ src/components/Project/Lang.js
@@ -6,7 +6,8 @@ const langImgs = {
     css: "https://img.shields.io/badge/CSS3-%231572B6.svg?&style=flat-square&logo=css3&logoColor=white",
     mysql: "https://img.shields.io/badge/MySQL-%23003B57.svg?&style=flat-square&logo=mysql&logoColor=white",
     mongodb: "https://img.shields.io/badge/MongoDB-%2347A248.svg?&style=flat-square&logo=mongodb&logoColor=white",
-    cpp: "https://img.shields.io/badge/C%2FC%2B%2B-%2300599C.svg?&style=flat-square&logo=C%2B%2B&logoColor=white"
+    cpp: "https://img.shields.io/badge/C%2FC%2B%2B-%2300599C.svg?&style=flat-square&logo=C%2B%2B&logoColor=white",
+    react: "https://img.shields.io/badge/React-%2320232a.svg?&style=flat-square&logo=react&logoColor=%2361DAFB"
 };
 
 export default function Lang({ lang }) {
