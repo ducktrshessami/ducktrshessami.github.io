8b52d2a10149148cfe381e872c788c38862247c7
ducktrshessami <jwee761@gmail.com>
2021-03-04 18:20:34 -0800

    fixed form handling for react

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,29 +1,49 @@
 import "./Contact.css";
 
+function getValues({ name, email, message }) {
+    return {
+        name: name.value,
+        email: email.value,
+        message: message.value
+    };
+}
+
+function validate({ name, email, message }) {
+    return name && /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email) && message;
+}
+
+function submit(event) {
+    let values = getValues(document.getElementById("contact-form"));
+    event.preventDefault();
+    if (validate(values)) {
+        // POST message
+    }
+}
+
 export default function Contact() {
     return (
-        <section class="row">
-            <div class="card blue-grey darken-4 white-text col s12 m8 offset-m2 l6 offset-l3">
-                <div class="card-content">
-                    <div class="hide-on-small-only">
-                        <h1 class="card-title">Contact</h1>
+        <section className="row">
+            <div className="card blue-grey darken-4 white-text col s12 m8 offset-m2 l6 offset-l3">
+                <div className="card-content">
+                    <div className="hide-on-small-only">
+                        <h1 className="card-title">Contact</h1>
                         <hr />
                         <br />
                     </div>
                     <form id="contact-form">
-                        <div class="input-field">
-                            <input type="text" id="name" name="name" class="validate white-text" />
-                            <label for="name">Name</label>
+                        <div className="input-field">
+                            <input type="text" id="name" name="name" className="validate white-text" />
+                            <label htmlFor="name">Name</label>
                         </div>
-                        <div class="input-field">
-                            <input type="text" name="email" id="email" class="validate white-text" />
-                            <label for="email">E-Mail</label>
+                        <div className="input-field">
+                            <input type="text" name="email" id="email" className="validate white-text" />
+                            <label htmlFor="email">E-Mail</label>
                         </div>
-                        <div class="input-field">
-                            <textarea name="message" id="message" class="materialize-textarea validate white-text"></textarea>
-                            <label for="message">Message</label>
+                        <div className="input-field">
+                            <textarea name="message" id="message" className="materialize-textarea validate white-text"></textarea>
+                            <label htmlFor="message">Message</label>
                         </div>
-                        <button type="submit" form="contact-form" id="submit" class="btn waves-effect waves-dark black-text">Submit</button>
+                        <button type="submit" form="contact-form" id="submit" className="btn waves-effect waves-dark black-text" onClick={submit}>Submit</button>
                     </form>
                 </div>
             </div>
