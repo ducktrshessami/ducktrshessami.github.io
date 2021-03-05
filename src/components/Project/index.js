0a86a0cc3ef54b8e7a264fc5b9a377395aaef05a
ducktrshessami <jwee761@gmail.com>
2021-03-04 17:35:58 -0800

    parse Project description with element keys

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,17 +1,24 @@
 import Lang from "./Lang";
 import "./Project.css";
 
+function parseDescription(description) {
+    let elems = [], lines = description.trim().split("\n");
+    for (let i = 0; i < lines.length; i++) {
+        elems.push(<p key={i}>{lines[i]}</p>);
+        elems.push(<br key={`${i}-br`} />)
+    }
+    return elems;
+}
+
 export default function Project({ title, description, langs, url }) {
-    let parsedDesc = description.trim().split('\n').map(line => `<p>${line.trim()}</p>`).join("\n<br>\n");
     return (
         <li className="portfolio-item">
-            <a href={url} className="portfolio-card card blue-grey darken-4 white-text waves-effect waves-dark">
+            <a href={url} className="project-card card blue-grey darken-4 white-text waves-effect waves-dark">
                 <div className="card-content">
                     <h3 className="card-title">{title}</h3>
                     <hr />
                     <br />
-                    {parsedDesc}
-                    <br />
+                    {parseDescription(description)}
                     <hr />
                     <ul className="project-langs">
                         {langs.map(lang => <Lang key={lang.trim()} lang={lang.trim()} />)}
