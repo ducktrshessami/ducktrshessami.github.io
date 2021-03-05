67bcbe800ec8f97a019b019ef224fa9c406591eb
ducktrshessami <jwee761@gmail.com>
2021-03-04 16:28:46 -0800

    Project page structure

--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -3,19 +3,31 @@ import Project from "../../components/Project";
 import "./Portfolio.css";
 
 const initialProjects = 6; // Number of projects displayed initially
+const projectIncrement = 6; // Project display increase per button press
 const clickMeSeconds = 5; // Number of seconds the intial help message is shown
 
 export default class Portfolio extends Component {
     state = {
-        shown: this.props.projects.slice(0, initialProjects),
-        all: this.props.projects
+        shown: initialProjects,
+        projects: this.props.projects
     }
 
-    button() {
-        if (this.state.shown.length !== this.state.all.length) {
+    showMore() {
+        this.setState({
+            shown: this.state.shown + projectIncrement,
+            projects: this.state.projects
+        });
+    }
+    
+    showAll() {
+
+    }
+
+    buttons() {
+        if (this.state.shown < this.state.projects.length) {
             return (
-                <div id="more-button-container" class="center">
-                    <button id="more-button" class="btn-large black-text">Show More</button>
+                <div id="more-button-container" className="center">
+                    <button id="more-button" className="btn-large black-text" onClick={this.showMore}>Show More</button>
                 </div>
             )
         }
@@ -23,10 +35,15 @@ export default class Portfolio extends Component {
 
     render() {
         return (
-            <section class="row">
-                <div class="col s12 m8 offset-m2">
-                    <ul id="portfolio-list">{this.state.shown.map(Project)}</ul>
-                    {this.button()}
+            <section className="row">
+                <div className="col s12 m8 offset-m2">
+                    <ul id="portfolio-list">
+                        {this.state.projects
+                            .slice(0, this.state.shown)
+                            .map(project => <Project key={project.title} {...project} />)
+                        }
+                    </ul>
+                    {this.buttons()}
                 </div>
             </section>
         );
