27825be1850460cdc0cf5e9d519577461e0aea31
ducktrshessami <jwee761@gmail.com>
2021-03-08 20:33:48 -0800

    getProjectList gist api call

--- src/utils/getProjectList.js
+++ src/utils/getProjectList.js
@@ -4,5 +4,9 @@ const gistID = "4d17eadd865d741676be7db15d4a7847";
 const gistEndpoint = "https://api.github.com/gists";
 
 export default function getProjectList() {
-
+    return phin({
+        url: gistEndpoint + "/" + gistID,
+        parse: "json"
+    })
+        .then(res => res.body);
 };
