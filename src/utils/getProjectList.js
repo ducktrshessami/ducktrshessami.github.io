d8899ce1d930ea2f384b8c93186175fb114eef22
ducktrshessami <jwee761@gmail.com>
2021-03-08 21:04:58 -0800

    use internal https module instead of phin

--- src/utils/getProjectList.js
+++ src/utils/getProjectList.js
@@ -1,12 +1,18 @@
-import phin from "phin";
+import https from "https";
 
 const gistID = "4d17eadd865d741676be7db15d4a7847";
 const gistEndpoint = "https://api.github.com/gists";
 
 export default function getProjectList() {
-    return phin({
-        url: gistEndpoint + "/" + gistID,
-        parse: "json"
+    return new Promise((resolve, reject) => {
+        https.get(gistEndpoint + "/" + gistID, res => {
+            let output = "";
+            res.on("data", chunk => output += chunk);
+            res.on("end", () => resolve(output));
+            res.on("error", reject);
+        });
     })
-        .then(res => res.body);
+        .then(JSON.parse)
+        .then(res => res.files["portfolio.json"].content)
+        .then(JSON.parse);
 };
