f7a20441c4979548628a8d47a5c7b7d06e68307d
ducktrshessami <jwee761@gmail.com>
2021-03-09 19:19:55 -0800

    moved sidenav initialization to sidenav subcomponent

--- src/components/Header/Sidenav.js
+++ src/components/Header/Sidenav.js
@@ -1,30 +1,38 @@
+import { Component } from "react";
 import {
     Switch,
     Route,
     Link
 } from "react-router-dom";
+import M from "materialize-css";
 import "./Sidenav.css";
 
-export default function Sidenav() {
-    return (
-        <ul id="sidenav" className="sidenav">
-            <Switch>
-                <Route path="/portfolio">
-                    <li><Link to="/">About Me</Link></li>
-                    <li><span className="disabled">Portfolio</span></li>
-                    <li><Link to="/contact">Contact</Link></li>
-                </Route>
-                <Route path="/contact">
-                    <li><Link to="/">About Me</Link></li>
-                    <li><Link to="/portfolio">Portfolio</Link></li>
-                    <li><span className="disabled">Contact</span></li>
-                </Route>
-                <Route path="/">
-                    <li><span className="disabled">About Me</span></li>
-                    <li><Link to="/portfolio">Portfolio</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
-                </Route>
-            </Switch>
-        </ul>
-    );
+export default class Sidenav extends Component {
+    componentDidMount() {
+        M.Sidenav.init(document.getElementById("sidenav"), { edge: "right" });
+    }
+
+    render() {
+        return (
+            <ul id="sidenav" className="sidenav">
+                <Switch>
+                    <Route path="/portfolio">
+                        <li><Link to="/">About Me</Link></li>
+                        <li><span className="disabled">Portfolio</span></li>
+                        <li><Link to="/contact">Contact</Link></li>
+                    </Route>
+                    <Route path="/contact">
+                        <li><Link to="/">About Me</Link></li>
+                        <li><Link to="/portfolio">Portfolio</Link></li>
+                        <li><span className="disabled">Contact</span></li>
+                    </Route>
+                    <Route path="/">
+                        <li><span className="disabled">About Me</span></li>
+                        <li><Link to="/portfolio">Portfolio</Link></li>
+                        <li><Link to="/contact">Contact</Link></li>
+                    </Route>
+                </Switch>
+            </ul>
+        );
+    }
 };
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,32 +1,24 @@
-import { Component } from "react";
 import { Link } from "react-router-dom";
-import M from "materialize-css";
 import Sidenav from "./Sidenav";
 
 import "./Header.css";
 
-export default class Header extends Component {
-    componentDidMount() {
-        M.Sidenav.init(document.getElementById("sidenav"), { edge: "right" });
-    }
-
-    render() {
-        return (
-            <header>
-                <nav className="nav-wrapper">
-                    <Link to="/" className="left hide-on-small-only">
-                        <img src="/logo192.png" alt="Logo" className="logo circle middle" />
-                        <span {...(this.props.route === "/" ? { className: "active"} : undefined)}>ducktrshessami</span>
-                    </Link>
-                    <ul className="left hide-on-small-only">
-                        <li><Link {...(this.props.route === "/portfolio" ? { className: "active"} : undefined)} to="/portfolio">Portfolio</Link></li>
-                        <li><Link {...(this.props.route === "/contact" ? { className: "active"} : undefined)} to="/contact">Contact</Link></li>
-                    </ul>
-                    <span className="brand-logo center hide-on-med-and-up">About Me</span>
-                    <div role="button" data-target="sidenav" className="sidenav-trigger right hide-on-med-and-up"><i className="material-icons">menu</i></div>
-                </nav>
-                <Sidenav />
-            </header>
-        );
-    }
+export default function Header({ route }) {
+    return (
+        <header>
+            <nav className="nav-wrapper">
+                <Link to="/" className="left hide-on-small-only">
+                    <img src="/logo192.png" alt="Logo" className="logo circle middle" />
+                    <span {...(route === "/" ? { className: "active" } : undefined)}>ducktrshessami</span>
+                </Link>
+                <ul className="left hide-on-small-only">
+                    <li><Link {...(route === "/portfolio" ? { className: "active" } : undefined)} to="/portfolio">Portfolio</Link></li>
+                    <li><Link {...(route === "/contact" ? { className: "active" } : undefined)} to="/contact">Contact</Link></li>
+                </ul>
+                <span className="brand-logo center hide-on-med-and-up">About Me</span>
+                <div role="button" data-target="sidenav" className="sidenav-trigger right hide-on-med-and-up"><i className="material-icons">menu</i></div>
+            </nav>
+            <Sidenav />
+        </header>
+    );
 };
