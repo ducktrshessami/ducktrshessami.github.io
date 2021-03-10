1a2b240be49054ee8e4cf261f9f879a2f4567ac7
ducktrshessami <jwee761@gmail.com>
2021-03-09 19:26:37 -0800

    close sidenav on link click

--- src/components/Header/Sidenav.js
+++ src/components/Header/Sidenav.js
@@ -8,8 +8,16 @@ import M from "materialize-css";
 import "./Sidenav.css";
 
 export default class Sidenav extends Component {
+    state = { instance: null }
+
     componentDidMount() {
-        M.Sidenav.init(document.getElementById("sidenav"), { edge: "right" });
+        this.setState({
+            instance: M.Sidenav.init(document.getElementById("sidenav"), { edge: "right" })
+        });
+    }
+
+    clicked() {
+        this.state.instance.close();
     }
 
     render() {
@@ -17,19 +25,19 @@ export default class Sidenav extends Component {
             <ul id="sidenav" className="sidenav">
                 <Switch>
                     <Route path="/portfolio">
-                        <li><Link to="/">About Me</Link></li>
+                        <li><Link to="/" onClick={() => this.clicked()}>About Me</Link></li>
                         <li><span className="disabled">Portfolio</span></li>
-                        <li><Link to="/contact">Contact</Link></li>
+                        <li><Link to="/contact" onClick={() => this.clicked()}>Contact</Link></li>
                     </Route>
                     <Route path="/contact">
-                        <li><Link to="/">About Me</Link></li>
-                        <li><Link to="/portfolio">Portfolio</Link></li>
+                        <li><Link to="/" onClick={() => this.clicked()}>About Me</Link></li>
+                        <li><Link to="/portfolio" onClick={() => this.clicked()}>Portfolio</Link></li>
                         <li><span className="disabled">Contact</span></li>
                     </Route>
                     <Route path="/">
                         <li><span className="disabled">About Me</span></li>
-                        <li><Link to="/portfolio">Portfolio</Link></li>
-                        <li><Link to="/contact">Contact</Link></li>
+                        <li><Link to="/portfolio" onClick={() => this.clicked()}>Portfolio</Link></li>
+                        <li><Link to="/contact" onClick={() => this.clicked()}>Contact</Link></li>
                     </Route>
                 </Switch>
             </ul>
