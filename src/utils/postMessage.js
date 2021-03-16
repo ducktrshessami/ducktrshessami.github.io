b107a11d32ffb15fea48a7b0f82ce3e43ef188f2
ducktrshessami <jwee761@gmail.com>
2021-03-15 22:09:53 -0700

    handle postMessage

--- src/utils/postMessage.js
+++ src/utils/postMessage.js
@@ -1,3 +1,7 @@
 export default function postMessage(data) {
-    return fetch();
+    return fetch("https://ducktrshessami-portfolio-api.herokuapp.com/messages", {
+        method: "post",
+        body: JSON.stringify(data),
+        headers: { 'Content-Type': 'application/json' }
+    });
 };
