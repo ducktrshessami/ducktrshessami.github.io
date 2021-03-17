cc786cf272374d4e476e09922c325bc34e766c56
Josh Fish <jwee761@gmail.com>
2021-03-17 16:32:16 -0700

    fleshed out project API util

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,11 +1,29 @@
 const endpoint = "https://ducktrshessami-portfolio-api.herokuapp.com/projects";
 const API = {
-    getAll
+    getAll,
+    addOne,
+    bulkAdd
 };
 
 function getAll() {
     return fetch(endpoint)
         .then(res => res.json());
+}
+
+function addOne(projectData) {
+    return fetch(endpoint + `?key=${key}`, {
+        method: "post",
+        body: JSON.stringify(projectData),
+        headers: { "Content-Type": "application/json" }
+    });
+}
+
+function bulkAdd(projectDataList, key) {
+    return fetch(endpoint + `/all?key=${key}`, {
+        method: "post",
+        body: JSON.stringify(projectDataList),
+        headers: { "Content-Type": "application/json" }
+    });
 }
 
 export default API;
