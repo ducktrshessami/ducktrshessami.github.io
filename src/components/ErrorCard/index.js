af2214db6cb39b0d5dccbde6d796110044085cbd
Josh Fish <jwee761@gmail.com>
2021-03-19 15:55:08 -0700

    fixed ErrorCard for just error

--- src/components/ErrorCard/index.js
+++ src/components/ErrorCard/index.js
@@ -9,13 +9,13 @@ function parseMessage(description) {
 
 export default function ErrorCard({ message }) {
     return (
-        <a href={url} className="card blue-grey darken-4 white-text waves-effect waves-dark">
+        <div className="card blue-grey darken-4 white-text waves-effect waves-dark">
             <div className="card-content">
                 <h3 className="card-title">Error</h3>
                 <hr />
                 <br />
-                {parseMessage(description)}
+                {parseMessage(message)}
             </div>
-        </a>
+        </div>
     );
 };
