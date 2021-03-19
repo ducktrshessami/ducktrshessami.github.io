d6ec26b0c83047ad5da482b2ee7b2eb961bb55ad
Josh Fish <jwee761@gmail.com>
2021-03-19 16:08:46 -0700

    dynamic Messages page

--- src/pages/Messages/index.js
+++ src/pages/Messages/index.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import Auth from "../../components/Auth";
 import MessageList from "../../components/MessageList";
+import Loading from "../../components/Loading";
+import ErrorCard from "../../components/ErrorCard";
 import API from "../../utils/API";
 
 export default function Messages() {
@@ -18,10 +20,25 @@ export default function Messages() {
             });
     }
 
+    function handleState() {
+        if (error) {
+            return <ErrorCard message={error} />;
+        }
+        else if (messages.length) {
+            return <MessageList messages={messages} />
+        }
+        else if (key) {
+            return <Loading />;
+        }
+        else {
+            return <Auth onSubmit={getMessages} />;
+        }
+    }
+
     return (
         <section className="row">
             <div className="col s12 m8 offset-m2 l6 offset-l3">
-                {key ? undefined : <Auth onSubmit={getMessages} />}
+                {handleState()}
             </div>
         </section>
     );
