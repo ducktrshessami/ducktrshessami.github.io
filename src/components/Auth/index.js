251b02194e47bf941674173efa51aa07f210b6c8
Josh Fish <jwee761@gmail.com>
2021-03-19 15:57:49 -0700

    created Auth component

---
+++ src/components/Auth/index.js
@@ -0,0 +1,23 @@
+export default function Auth({ onSubmit }) {
+    function submitWrapper(event) {
+        event.preventDefault();
+        let password = document.getElementById("password").value.trim();
+        if (password) {
+            onSubmit(password);
+        }
+    }
+
+    return (
+        <div className="card blue-grey darken-4 white-text">
+            <div className="card-content">
+                <form id="contact-form" onSubmit={submitWrapper}>
+                    <div className="input-field">
+                        <input type="text" id="password" name="password" className="validate white-text" />
+                        <label htmlFor="password">Password</label>
+                    </div>
+                    <button type="submit" form="contact-form" className="btn waves-effect waves-dark black-text submit-button">Get Messages</button>
+                </form>
+            </div>
+        </div>
+    );
+};
