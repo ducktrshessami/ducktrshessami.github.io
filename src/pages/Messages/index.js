0ef3e0c751a4fd0cec642c4f1d369cf4d9248b86
Josh Fish <jwee761@gmail.com>
2021-03-19 14:54:29 -0700

    render auth in messages

--- src/pages/Messages/index.js
+++ src/pages/Messages/index.js
@@ -1,5 +1,28 @@
+import { useState } from "react";
+import Auth from "../../components/Auth";
+import MessageList from "../../components/MessageList";
+import API from "../../utils/API";
+
 export default function Messages() {
+    const [key, setKey] = useState("");
+    const [error, setError] = useState("");
+    const [messages, setMessages] = useState([]);
+
+    function getMessages(key) {
+        setKey(key);
+        API.messages.getAll(key)
+            .then(setMessages)
+            .catch(err => {
+                console.error(err);
+                setError("Failed to fetch messages");
+            });
+    }
+
     return (
-        <section></section>
+        <section className="row">
+            <div className="col s12 m8 offset-m2 l6 offset-l3">
+                {key ? undefined : <Auth onSubmit={getMessages} />}
+            </div>
+        </section>
     );
 };
