6a82bec35036a3bc30cd625f5ad651eda08677e4
ducktrshessami <jwee761@gmail.com>
2021-03-29 02:25:47 -0700

    convert About to class component to ensure single bio get with text file

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,40 +1,61 @@
-import { useState } from "react";
+import { Component } from "react";
 import Bio from "../../components/Bio";
 import Socials from "../../components/Socials";
-import API from "../../utils/API";
+import bioFile from "../../bio";
 import "./About.css";
 
-export default function About() {
-    const [bioText, setBioText] = useState("");
-    let logoUrl = process.env.PUBLIC_URL + "/logo512.png";
+export default class About extends Component {
+    state = {
+        logoUrl: process.env.PUBLIC_URL + "/logo512.png",
+        bioText: "",
+        errorText: ""
+    }
 
-    API.getBio()
-        .then(setBioText)
-        .catch(console.error);
+    componentDidMount() {
+        fetch(bioFile)
+            .then(res => {
+                if (res.ok) {
+                    return res.text()
+                }
+            })
+            .then((bio = "") => this.setState({
+                ...this.state,
+                bioText: bio
+            }))
+            .catch(err => {
+                console.error(err);
+                this.setState({
+                    ...this.state,
+                    errorText: "Could not fetch bio"
+                });
+            });
+    }
 
-    return (
-        <article className="row">
-            <div className="col s12 m8 offset-m2 white-text">
-                <div className="card blue-grey darken-4 horizontal hide-on-small-only">
-                    <img src={logoUrl} alt="Logo" className="card-image horizontal-image" />
-                    <div className="card-stacked">
+    render() {
+        return (
+            <article className="row">
+                <div className="col s12 m8 offset-m2 white-text">
+                    <div className="card blue-grey darken-4 horizontal hide-on-small-only">
+                        <img src={this.state.logoUrl} alt="Logo" className="card-image horizontal-image" />
+                        <div className="card-stacked">
+                            <div className="card-content">
+                                <h1 className="card-title">About Me</h1>
+                                <hr />
+                                <br />
+                                <Bio text={this.state.errorText || this.state.bioText} />
+                            </div>
+                            <Socials />
+                        </div>
+                    </div>
+                    <div className="card blue-grey darken-4 hide-on-med-and-up">
+                        <img src={this.state.logoUrl} alt="Logo" className="card-image vertical-image" />
                         <div className="card-content">
-                            <h1 className="card-title">About Me</h1>
-                            <hr />
-                            <br />
-                            <Bio text={bioText} />
+                            <Bio text={this.state.errorText || this.state.bioText} />
                         </div>
                         <Socials />
                     </div>
                 </div>
-                <div className="card blue-grey darken-4 hide-on-med-and-up">
-                    <img src={logoUrl} alt="Logo" className="card-image vertical-image" />
-                    <div className="card-content">
-                        <Bio text={bioText} />
-                    </div>
-                    <Socials />
-                </div>
-            </div>
-        </article>
-    );
-};
+            </article>
+        );
+    }
+}
