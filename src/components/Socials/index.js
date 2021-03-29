8657c98ca06b58711d9faedddc6a2d708ccf5b00
ducktrshessami <jwee761@gmail.com>
2021-03-29 01:15:00 -0700

    moved social list to separate component

---
+++ src/components/Socials/index.js
@@ -0,0 +1,11 @@
+export default function Socials() {
+    return (
+        <ul className="card-content">
+            <li><a href="https://github.com/ducktrshessami">GitHub</a></li>
+            <li><a href="https://greasyfork.org/en/users/3488-ducktrshessami">Greasy Fork</a></li>
+            <li><a href="https://www.linkedin.com/in/josh-wee-b90455106/">LinkedIn</a></li>
+            <li>Email: <a href="mailto:jwee761@gmail.com">jwee761@gmail.com</a></li>
+            <li>Discord: ducktrshessami#4884</li>
+        </ul>
+    );
+};
--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,4 +1,5 @@
 import Bio from "../../components/Bio";
+import Socials from "../../components/Socials";
 import "./About.css";
 
 export default function About() {
@@ -15,13 +16,7 @@ export default function About() {
                             <br />
                             <Bio />
                         </div>
-                        <ul className="card-content">
-                            <li><a href="https://github.com/ducktrshessami">GitHub</a></li>
-                            <li><a href="https://greasyfork.org/en/users/3488-ducktrshessami">Greasy Fork</a></li>
-                            <li><a href="https://www.linkedin.com/in/josh-wee-b90455106/">LinkedIn</a></li>
-                            <li>Email: <a href="mailto:jwee761@gmail.com">jwee761@gmail.com</a></li>
-                            <li>Discord: ducktrshessami#4884</li>
-                        </ul>
+                        <Socials />
                     </div>
                 </div>
                 <div className="card blue-grey darken-4 hide-on-med-and-up">
@@ -29,13 +24,7 @@ export default function About() {
                     <div className="card-content">
                         <Bio />
                     </div>
-                    <ul className="card-content">
-                        <li><a href="https://github.com/ducktrshessami">GitHub</a></li>
-                        <li><a href="https://greasyfork.org/en/users/3488-ducktrshessami">Greasy Fork</a></li>
-                        <li><a href="https://www.linkedin.com/in/josh-wee-b90455106/">LinkedIn</a></li>
-                        <li>Email: <a href="mailto:jwee761@gmail.com">jwee761@gmail.com</a></li>
-                        <li>Discord: ducktrshessami#4884</li>
-                    </ul>
+                    <Socials />
                 </div>
             </div>
         </article>
