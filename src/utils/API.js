4983b3cdf8fcd757a72c23a9bb9f9bd23a0438a9
ducktrshessami <jwee761@gmail.com>
2021-11-08 12:53:00 -0800

    removed unused content

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,13 +1,6 @@
 const URL = "https://ducktrshessami-portfolio-api.herokuapp.com";
 
 const API = {
-    sendMessage: function (messageData) {
-        return fetch(URL + "/message", {
-            method: "post",
-            body: JSON.stringify(messageData),
-            headers: { "Content-Type": "application/json" }
-        });
-    },
     getLangs: function () {
         return fetch(URL + "/langs")
             .then(res => res.json());
