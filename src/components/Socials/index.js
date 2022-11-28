a8e36079b3d6d00120bddb40ae5fd6a34f50ab88
ducktrshessami <jwee761@gmail.com>
2022-11-27 18:03:59 -0800

    socials margins

--- src/components/Socials/index.js
+++ src/components/Socials/index.js
@@ -1,6 +1,8 @@
+import "./Socials.css";
+
 export default function Socials() {
     return (
-        <ul className="card-content">
+        <ul className="card-content socials-list">
             <li><a href="https://github.com/ducktrshessami">GitHub</a></li>
             <li><a href="https://www.linkedin.com/in/josh-wee-b90455106/">LinkedIn</a></li>
             <li>Email: <a href="mailto:jwee761@gmail.com">jwee761@gmail.com</a></li>
