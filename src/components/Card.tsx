5a3749a19569a7d808c35b312ad64325fc7248ce
ducktrshessami <jwee761@gmail.com>
2024-02-10 17:29:36 -0800

    refactor: cards as component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -16,44 +16,44 @@ const Card = React.forwardRef<
 ));
 Card.displayName = "Card";
 
-const CardHeader = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn("flex flex-col space-y-1.5 p-6", className)}
-    {...props}
-  />
-));
-CardHeader.displayName = "CardHeader";
+// const CardHeader = React.forwardRef<
+//   HTMLDivElement,
+//   React.HTMLAttributes<HTMLDivElement>
+// >(({ className, ...props }, ref) => (
+//   <div
+//     ref={ref}
+//     className={cn("flex flex-col space-y-1.5 p-6", className)}
+//     {...props}
+//   />
+// ));
+// CardHeader.displayName = "CardHeader";
 
-const CardTitle = React.forwardRef<
-  HTMLParagraphElement,
-  React.HTMLAttributes<HTMLHeadingElement>
->(({ className, ...props }, ref) => (
-  <h3
-    ref={ref}
-    className={cn(
-      "text-2xl font-semibold leading-none tracking-tight",
-      className
-    )}
-    {...props}
-  />
-));
-CardTitle.displayName = "CardTitle";
+// const CardTitle = React.forwardRef<
+//   HTMLParagraphElement,
+//   React.HTMLAttributes<HTMLHeadingElement>
+// >(({ className, ...props }, ref) => (
+//   <h3
+//     ref={ref}
+//     className={cn(
+//       "text-2xl font-semibold leading-none tracking-tight",
+//       className
+//     )}
+//     {...props}
+//   />
+// ));
+// CardTitle.displayName = "CardTitle";
 
-const CardDescription = React.forwardRef<
-  HTMLParagraphElement,
-  React.HTMLAttributes<HTMLParagraphElement>
->(({ className, ...props }, ref) => (
-  <p
-    ref={ref}
-    className={cn("text-sm text-gray-500 dark:text-gray-400", className)}
-    {...props}
-  />
-));
-CardDescription.displayName = "CardDescription";
+// const CardDescription = React.forwardRef<
+//   HTMLParagraphElement,
+//   React.HTMLAttributes<HTMLParagraphElement>
+// >(({ className, ...props }, ref) => (
+//   <p
+//     ref={ref}
+//     className={cn("text-sm text-gray-500 dark:text-gray-400", className)}
+//     {...props}
+//   />
+// ));
+// CardDescription.displayName = "CardDescription";
 
 const CardContent = React.forwardRef<
   HTMLDivElement,
@@ -63,16 +63,32 @@ const CardContent = React.forwardRef<
 ));
 CardContent.displayName = "CardContent";
 
-const CardFooter = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn("flex items-center p-6 pt-0", className)}
-    {...props}
-  />
-));
-CardFooter.displayName = "CardFooter";
+// const CardFooter = React.forwardRef<
+//   HTMLDivElement,
+//   React.HTMLAttributes<HTMLDivElement>
+// >(({ className, ...props }, ref) => (
+//   <div
+//     ref={ref}
+//     className={cn("flex items-center p-6 pt-0", className)}
+//     {...props}
+//   />
+// ));
+// CardFooter.displayName = "CardFooter";
 
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent };
+export interface PortfolioCardProps {
+  name: string;
+  description: string;
+}
+
+export function PortfolioCard(props: PortfolioCardProps) {
+  return (
+    <Card className="w-80 py-4">
+      <CardContent className="flex flex-col space-y-4">
+        <div className="space-y-2">
+          <h3 className="text-lg font-bold">{props.name}</h3>
+          <p className="text-sm text-gray-500 dark:text-gray-400">{props.description}</p>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
